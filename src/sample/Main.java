4497c0b607eb59cc41e6ce241c7fc44df89e1863
bex3 <Rebeccam.bearden@gmail>
2016-09-01 11:11:52 -0400

    first

---
+++ src/sample/Main.java
@@ -0,0 +1,111 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    final double DEFAULT_SCENE_WIDTH = 800;
+    final double DEFAULT_SCENE_HEIGHT = 600;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+
+        //we're using a grid layout
+        GridPane grid = new  GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        grid.setGridLinesVisible(true);
+        //ADD STUFF FROM TIYO
+
+        // add buttons and canvas to the grid
+        Text sceneTitle = new Text("Welcome to Paint application");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0);
+
+        Button button = new Button("Sample paint button");
+        HBox hbButton = new HBox(10);
+        hbButton.setAlignment(Pos.TOP_LEFT);
+        hbButton.getChildren().add(button);
+        grid.add(hbButton, 0, 1); //column first then row
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("I can switch to another scene here ...");
+//                primaryStage.setScene(loginScene);
+//                startSecondStage();
+            }
+        });
+
+        // add canvas
+        Canvas canvas = new Canvas(DEFAULT_SCENE_WIDTH, DEFAULT_SCENE_HEIGHT-100);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
+        gc.setLineWidth(5);
+
+        canvas.setOnMouseMoved(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent e) {
+//                System.out.println("x: " + e.getX() + ", y: " + e.getY());
+                gc.strokeOval(e.getX(), e.getY(), 10, 10);
+//                addStroke(e.getX(), e.getY(), 10);
+            }
+        });
+
+        canvas.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent e) {
+                System.out.println(e.getCode());
+                System.out.println(e.getText());
+            }
+        });
+
+
+        grid.add(canvas, 0, 2);
+
+
+
+
+        // set iyr grid layout on the scene
+        Scene defaultScene = new Scene (grid, DEFAULT_SCENE_WIDTH, DEFAULT_SCENE_HEIGHT);
+
+
+        primaryStage.setScene(defaultScene);
+        primaryStage.show();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
